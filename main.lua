7e1e3402114679ec697546b2b56a6dcc4ea977cc
Serg <serhiipoklonsky2@gmail.com>
2025-11-02 15:24:00 -0500

    v1
    Added WindUI for creating a user interface with settings.

--- main.lua
+++ main.lua
@@ -1,1 +1,29 @@
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
+local Window = WindUI:CreateWindow({
+    Title = "Wind Scripts",
+    Icon = "shopping-cart",
+    Author = "by Serhii",
+    Folder = "wind-scripts",
+    Transparent = true,
+    Theme = "Dark",
+    
+    User = {
+        Enabled = true,
+        Anonymous = false,
+    },
+})
+
+local TabSettings = Window:Tab({
+    Title = "Settings",
+    Icon = "cog"
+})
+
+TabSettings:Keybind({
+    Title = "UI Keybind",
+    Desc = "Open/close keybind",
+    Value = "RightAlt",
+    Callback = function(v)
+        Window:SetToggleKey(Enum.KeyCode[v])
+    end
+})
